00c43412f1339726169f39efec443d6bc133ba98
whjwjx <whjwjx@gmail.com>
2026-03-31 15:09:20 +0800

    style(Card): 统一图片容器的类名顺序
    按照 Tailwind CSS 推荐顺序调整类名，提升代码可维护性。

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,7 +14,7 @@ const Card = ({ title, description, imgSrc, href }) => (
             <Image
               alt={title}
               src={imgSrc}
-              className="object-cover object-center w-full h-auto"
+              className="h-auto w-full object-cover object-center"
               width={544}
               height={306}
             />
@@ -23,7 +23,7 @@ const Card = ({ title, description, imgSrc, href }) => (
           <Image
             alt={title}
             src={imgSrc}
-            className="object-cover object-center w-full h-auto"
+            className="h-auto w-full object-cover object-center"
             width={544}
             height={306}
           />
